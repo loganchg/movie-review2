22dc476772be5e467b34b1c33a0f60281a2a824a
JAGx9 <jagonzalezf9@gmail.com>
2022-08-17 16:40:03 -0700

    created backend and connected to mongodb atlas

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express  from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes)
+
+
+app.use(bodyParser.json({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+
+const CONNECTION_URL ='mongodb+srv://JAGx9:Moviereview123@cluster0.havbrtt.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5001;
+
+mongoose.connect(CONNECTION_URL)
+    .then(()=> app.listen(PORT, () => console.log(`Server running: ${PORT}`)))
+    .catch((error)=> console.log(error.message));
+
+
+
